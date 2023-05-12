b40559c57960cf36f7612dc051ee924b5416f236
Jssice <jiaxiaobin@hotmail.com>
2023-05-12 16:14:11 +0930

    the answer

--- main.go
+++ main.go
@@ -69,6 +69,9 @@ func main() {
 	staticFs := http.FileServer(getStaticFiles())
 
 	mux := http.NewServeMux()
+	the_answer:=42
+	fmt.Printf("The answer is %s\n the_answer")
+
 
 	// routes
 	mux.HandleFunc("/api", receiveJSONHandler)
